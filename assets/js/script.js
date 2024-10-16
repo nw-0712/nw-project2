6d2a5b8574f069491f82f99a6126e7e8aef1134b
Niccola Winter <niccola.winter@students.codeinstitute.net>
2024-10-16 18:31:57 +0000

    added tile flipping section in html, and jS section mapping

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Memory match logic section //
+
+
+//Moves left section//
+
+
+//Start game section//
+
+
+//Initialise game section//
+
+
+//Tile values//
+
+
+//Tile grid layout//
+
+
+//Flip tile section//
+
+
+//Tile match section//
+
+
+//Timer-counter section//
+
+
+//End game section//
+
+
+
+//Notification section//
